0b73b5986a97e5fd642b0be1796eb78cf89da9e1
Aayush jindal <aayushjindal096@gmail.com>
2020-01-29 19:13:31 +0530

    Initial Commit

---
+++ DynamicTextView/app/src/main/java/com/journaldev/androidautosizingtextview/MainActivity.java
@@ -0,0 +1,38 @@
+package com.journaldev.androidautosizingtextview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final TextView txtAutoSizeTextViewDynamic = findViewById(R.id.autoTextViewDynamic);
+        EditText editText = findViewById(R.id.editText);
+
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                txtAutoSizeTextViewDynamic.setText(charSequence.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+    }
+}
