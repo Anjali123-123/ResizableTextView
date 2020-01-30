13dbd39e2f2b3b343992137c11416cd25d60514b
Anjali Jindal <anjalijindal.213@gmail.com>
2020-01-30 08:06:37 +0530

    update

--- DynamicTextView/app/src/main/java/com/journaldev/androidautosizingtextview/MainActivity.java
+++ DynamicTextView/app/src/main/java/com/journaldev/androidautosizingtextview/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+//final
         final TextView txtAutoSizeTextViewDynamic = findViewById(R.id.autoTextViewDynamic);
         EditText editText = findViewById(R.id.editText);
 
